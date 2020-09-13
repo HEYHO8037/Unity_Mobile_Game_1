56bd2b7ad13e42fdaaa953b9068e00f031f51736
HEYHO8037 <HEYHO8037@users.noreply.github.com>
2020-09-13 16:34:48 +0900

    유니티_백그라운드이동_x축고정
    카메라 좌표값을 가져온뒤 x축을 0으로 고정시킴

---
+++ BackGround_Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround_Move : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+
+    [SerializeField]
+    private GameObject Player;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Player == true)
+        {
+            Vector3 p_Camera;
+            p_Camera = mainCamera.transform.position;
+            p_Camera.x = 0;
+            p_Camera.z = 0;
+
+            this.transform.position = p_Camera;
+        }
+        else if (Player == false)
+        {
+            this.transform.position = this.transform.position;
+        }
+    }
+}
